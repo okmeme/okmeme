f1382acd370fffa699f8a6e83af5dd0367be2e11
Adam Chao <adam@thinkaliker.com>
2015-07-03 22:49:50 -0700

    okmeme

---
+++ js/meme.js
@@ -0,0 +1,28 @@
+$(function() {
+  var ok = 1;
+  console.log("init");
+
+  $("#meme").click(function() {
+    console.log("pressed " + ok);
+    alert("me too thanks");
+    ok++;
+    if (ok > 2) {
+      $("#meme").text("i don't know what you expected");
+    }
+    if (ok > 5) {
+      $("#meme").text("pls stop");
+      $("#meme").addClass("btn-warning");
+      $("#meme").removeClass("btn-success");
+    }
+    if (ok > 10) {
+      $("#meme").text("fine");
+      $("#meme").addClass("btn-info");
+      $("#meme").removeClass("btn-warning");
+    }
+    if (ok > 20) {
+      alert("go away");
+      $("#meme").hide();
+    }
+  });
+
+});
