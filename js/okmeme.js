e9c38f1a2ea2c2b157d54b908c6d7528b52f0763
Adam Chao <adam@thinkaliker.com>
2015-07-05 23:24:50 -0700

    fixed formatting

--- js/okmeme.js
+++ js/okmeme.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
   });
 
   $(document).on('click', '.expand', function(e) {
-        $(this).removeClass("expand").addClass("contract");
-      
-        var  domelem = $(this).parent().next('.link');
-        var cuck = domelem.clone(true).empty().embedly().insertAfter(domelem);
+    $(this).removeClass("expand").addClass("contract");
+
+    var domelem = $(this).parent().next('.link');
+    var cuck = domelem.clone(true).empty().embedly().insertAfter(domelem);
   });
 
   $(document).on('click', '.contract', function(e) {
-        $(this).removeClass("contract").addClass("expand");
-        var  domelem = $(this).parent().next().next().remove();
+    $(this).removeClass("contract").addClass("expand");
+    var domelem = $(this).parent().next().next().remove();
   });
 
 });
