4080d9965b21b18e79d4fbe9c5854ade9e6988b0
Alexander Fu <aafu@ucdavis.edu>
2015-07-05 18:28:39 -0700

    Update okmeme.js

--- js/okmeme.js
+++ js/okmeme.js
@@ -1,5 +1,6 @@
-$(function() {
-  $(".commentsection").hide();
+
+$( document ).ready(function() {
+      $(".commentsection").hide();
   $(".commentlink").click(function() {
     $(this).next(".commentsection").slideToggle(200);
   });
