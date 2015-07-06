216b8873b4f9b2f7c3fe1d367c6538befc620b39
Adam Chao <adam@thinkaliker.com>
2015-07-05 21:50:31 -0700

    embedly stuff

--- js/okmeme.js
+++ js/okmeme.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
+  $.embedly.defaults.key = '31ff5a4877c74183ad6c22e84de3d359';
+
   $(".commentsection").hide();
   $(".commentlink").click(function() {
     $(this).parent().parent().parent().next('.commentsection').slideToggle(200);
   });
-});
 
+
+  $(".expand").click(function() {
+    if ($(this).parent().next(".embed").length) {
+      $(this).parent().next('.link').removeClass("hide");
+      $(this).parent().next(".embed").remove();
+      console.log("show link");
+    } else {
+      $(this).parent().next('.link').clone(true);
+        //.embedly();
+      $(this).parent().next('.link').addClass("hide");
+      console.log("embedly");
+    }
+  });
+});
